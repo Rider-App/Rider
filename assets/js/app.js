e95a1e0841ed3b30961bfc710baacd5ab9674cb7
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-04-04 13:17:27 -0400

    files created and routing is set up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,30 @@
+var riderApp = angular.module('riderApp', ['ngRoute']);
 
+
+riderApp.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'home.html',
+            })
+            .when('/Login',  {
+                templateUrl : 'login.html',
+            })
+            .when('/SignUp', {
+                templateUrl : 'sign-up.html',
+            })
+            .when('/Rates', {
+                templateUrl : 'rates.html',
+            })
+            .when('/LearnMore', {
+                templateUrl : 'learn-more.html',
+            })
+            .when('/Ridesharing', {
+                templateUrl : 'ridesharing.html',
+            })
+            .when('/Taxi', {
+                templateUrl : 'taxi.html',
+            })
+            .when('/PublicTransit', {
+                templateUrl : 'public-transit.html',
+            })
+          }); //closes riderApp routeProvider
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,1 +1,4 @@
-
+riderApp.controller('mainController', function($scope) {
+    console.log("controller is working")
+    $scope.message = 'HELLO WORLD';
+  });
