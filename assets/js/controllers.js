cebe0b7eb629452a2e8b8825c74b31332682611c
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-04-05 17:51:40 -0400

    html progress

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,8 +3,10 @@ riderApp.controller('mainController', [ '$http', '$scope', function($http, $scop
   $scope.message = 'HELLO WORLD';
 
   function init() {
-    var input = document.getElementById('searchTextField');
-    var autocomplete = new google.maps.places.Autocomplete(input);
+    var inputStart = document.getElementById('searchTextField');
+    var inputDest = document.getElementById('searchTextFieldTwo');
+    var autocomplete = new google.maps.places.Autocomplete(inputStart);
+    var autocompleteTwo = new google.maps.places.Autocomplete(inputDest);
   }
 
   google.maps.event.addDomListener(window, 'load', init);
