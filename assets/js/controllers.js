8a6b6d00d3811b015298cb58bfaaf08f1c96e07f
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-04-05 09:35:43 -0400

    fixing google places autocomplete in home page

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,4 +1,13 @@
-riderApp.controller('mainController', function($scope) {
-    console.log("controller is working")
-    $scope.message = 'HELLO WORLD';
-  });
+
+riderApp.controller('mainController', [ '$http', '$scope', function($http, $scope){
+  $scope.message = 'HELLO WORLD';
+
+  function init() {
+    var input = document.getElementById('searchTextField');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+  }
+
+  google.maps.event.addDomListener(window, 'load', init);
+  $scope.$apply();
+
+}]);
