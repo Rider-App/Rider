0fb3d1adce73a546b292648dabd554648849e3e6
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-04-06 11:16:27 -0400

    able to call from the farefairy api

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,6 +1,5 @@
 
 riderApp.controller('mainController', [ '$http', '$scope', function($http, $scope){
-  $scope.message = 'HELLO WORLD';
 
   function init() {
     var inputStart = document.getElementById('searchTextField');
@@ -12,8 +11,16 @@ riderApp.controller('mainController', [ '$http', '$scope', function($http, $scop
   google.maps.event.addDomListener(window, 'load', init);
   // $scope.$apply();
 
+
+}]);
+
+riderApp.controller('farefairyController', [ '$http', '$scope', function($http, $scope){
+  $scope.message = 'HELLO WORLD';
+
   $http.get('http://farefairy.herokuapp.com/?origin=5512%20Bridgeman%20Ct%20Durham%20NC%2027703&destination=334%20Blackwell%20Street%20B017,%20Durham,%20NC%2027701').success(function(data){
-    console.log('scope is working');
-);
+    $scope.farefairy = data;
+    console.log(data);
+
+})
 
 }]);
